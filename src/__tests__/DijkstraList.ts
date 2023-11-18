083e40b01ab7fab2221d2816917fec8e224d5196
goncalodasilva <gnunosilva@hotmail.com>
2023-11-18 14:49:13 +0100

    setting up dijk-minHeap & bfsGraphList

--- src/__tests__/DijkstraList.ts
+++ src/__tests__/DijkstraList.ts
@@ -1,4 +1,5 @@
 import dijkstra_list from "@code/DijkstraList";
+import dijkstra_list_min_heap from "@code/DijkstraListMinHeap";
 import { list1 } from "./graph";
 
 test("dijkstra via adj list", function () {
@@ -6,4 +7,8 @@ test("dijkstra via adj list", function () {
     // what..
     // what...
     expect(dijkstra_list(0, 6, list1)).toEqual([0, 1, 4, 5, 6]);
+});
+
+test("dijkstra via adj list - using a minHeap", function () {
+    expect(dijkstra_list_min_heap(0, 6, list1)).toEqual([0, 1, 4, 5, 6]);
 });
--- src/day1/BFSGraphList.ts
+++ src/day1/BFSGraphList.ts
@@ -1,3 +1,17 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+import Queue from "./Queue";
 
+export default function bfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const prev: number[] = new Array(graph.length).fill(-1);
+    const q: Queue<number> = new Queue();
+    q.enqueue(source);
+    seen[source] = true;
+    
+    do {
+        const node = q.deque()
+        if (node === needle) {
+            
+
+        }
+    } while (q.length)
 }
